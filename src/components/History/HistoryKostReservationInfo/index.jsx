e255b7bd3ffe82ab1540ed35a495f619e80f7dfb
petrusceles <petrusprihantoro@gmail.com>
2023-03-09 16:22:15 +0700

    reservation page done

--- src/components/History/HistoryKostReservationInfo/index.jsx
+++ src/components/History/HistoryKostReservationInfo/index.jsx
@@ -5,7 +5,7 @@ import { ArrowRightCircleIcon } from "@heroicons/react/24/outline";
 import HistorySidebar from "../historySidebar";
 function HistoryKostReservationInfo() {
   return (
-    <div className="pt-[88px]">
+    <div className="pt-[88px] lg:pt-32">
       <div className="container px-7 flex flex-wrap w-full gap-5">
         {/* Breadcrumbs */}
         <div className="text-xs font-medium breadcrumbs flex items-center w-full lg:text-base">
@@ -23,71 +23,88 @@ function HistoryKostReservationInfo() {
         </div>
 
         {/* SIdebar */}
-        <HistorySidebar />
+        <div className="w-full flex gap-8 ">
+          <HistorySidebar />
 
-        <div className="flex flex-wrap w-[50%] py-2 gap-2">
-          {/* Kost Brief */}
-          <div className="flex w-full gap-2 flex-wrap border-b-2 pb-3">
-            <div className="w-full  overflow-hidden h-32 rounded flex items-center ">
-              <img
-                src="https://res.cloudinary.com/dqzqbgi8e/image/upload/v1678116364/NaqosV2/Kost/matt-reames-aFk1RLZgJTs-unsplash_j7dsh2.jpg"
-                className="object-cover object-center"
-              />
-            </div>
-            <div className="flex items-center justify-between w-full gap-2">
-              <div className="h-full flex items-center bg-secondary justify-center rounded py-2">
-                <div className="flex flex-wrap w-[90%]">
-                  <p className="text-sx font-semibold h-fit w-full">
-                    Kos Alamanda
-                  </p>
-                  <div className="flex gap-1 h-fit w-full">
-                    <MapPinIcon className="w-3 " />
-                    <p className="text-[11px]">Yogyakarta</p>
+          <div className="flex flex-wrap w-full py-2 gap-3 lg:w-full lg:gap-6 ">
+            {/* Kost Brief */}
+            <div className="flex w-full gap-5 flex-wrap border-b-2 pb-5 items-center lg:pb-7">
+              <div className="w-[40%] overflow-hidden h-28 rounded flex items-center lg:w-[30%] lg:h-36">
+                <img
+                  src="https://res.cloudinary.com/dqzqbgi8e/image/upload/v1678116364/NaqosV2/Kost/matt-reames-aFk1RLZgJTs-unsplash_j7dsh2.jpg"
+                  className="object-cover object-center"
+                />
+              </div>
+              <div className="flex justify-between w-[55%] gap-2 items-start flex-wrap">
+                <div className="grid-cols-1 grid gap-1 w-full">
+                  <h2 className="font-semibold text-sm lg:text-lg">Kos Alamanda</h2>
+                  <div className="flex text-xs gap-1 items-center lg:text-base">
+                    <MapPinIcon className="w-3 lg:w-5" />
+                    <p>Yogyakarta</p>
                   </div>
-                  <p className="text-[11px] h-fit items-center flex w-full">
-                    Booking ID: 00000000
-                  </p>
+                  <p className="text-xs lg:text-base">Booking ID : 00000000</p>
                 </div>
-              </div>
-              <div className="flex flex-wrap justify-center justify-self-center ">
-                <div className="grid grid-cols-1 justify-items-center w-full">
-                  <p className="text-[8px]">Check in</p>
-                  <p className="text-[11px]">Jan 17, 23</p>
-                </div>
-                <ArrowRightCircleIcon className="w-4" />
-                <div className="grid grid-cols-1 justify-items-center w-full">
-                  <p className="text-[8px]">Check out</p>
-                  <p className="text-[11px]">Jan 24, 23</p>
+                <div className="flex flex-wrap gap-2 w-full">
+                  <div className="grid grid-cols-1 justify-items-center">
+                    <p className="text-[8px] lg:text-xs">Check in</p>
+                    <p className="text-[11px] lg:text-sm">Jan 17, 23</p>
+                  </div>
+                  <ArrowRightCircleIcon className="w-4 lg:w-6" />
+                  <div className="grid grid-cols-1 justify-items-center">
+                    <p className="text-[8px] lg:text-xs">Check out</p>
+                    <p className="text-[11px] lg:text-sm">Jan 24, 23</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          {/* Payment Info */}
-          <div className="w-full flex flex-wrap gap-2">
-            <h1 className="text-sm font-semibold">Rincian Pembayaran</h1>
-            <div className="w-full flex flex-wrap p-3 bg-secondary rounded gap-2">
-              <div className="text-[11px] w-full gap-1 flex flex-wrap border-b-2 pb-2 border-slate-500 font-semibold">
-                <div className="w-full flex justify-between">
-                  <p>Harga kos</p>
-                  <p>
-                    Rp300.000
-                    <span className="text-[9px] font-normal">/minggu</span>
-                  </p>
-                </div>
-                <div className="w-full flex justify-between">
-                  <p>Waktu sewa</p>
-                  <p>1 minggu</p>
+            <div className="w-full flex flex-wrap gap-6 pb-8 lg:gap-9">
+              {/* Payment Info */}
+              <div className="w-full flex flex-wrap gap-2 lg:gap-5">
+                <h1 className=" font-semibold lg:text-2xl">Rincian Pembayaran</h1>
+                <div className="w-full flex flex-wrap py-5 px-4 bg-secondary rounded-lg gap-3 lg:px-8 lg:gap-5">
+                  <div className="text-sm w-full gap-3 flex flex-wrap border-b-2 pb-3 border-primary font-medium lg:text-lg lg:gap-5 lg:pb-5">
+                    <div className="w-full flex justify-between">
+                      <p>Harga kos</p>
+                      <p className="font-bold">
+                        Rp300.000
+                        <span className="text-[11px] font-normal lg:text-sm">/minggu</span>
+                      </p>
+                    </div>
+                    <div className="w-full flex justify-between">
+                      <p>Waktu sewa</p>
+                      <p>1 minggu</p>
+                    </div>
+                  </div>
+                  <div className="w-full flex justify-between text-sm font-semibold lg:text-lg">
+                    <p>Total Pembayaran</p>
+                    <p>Rp300.000</p>
+                  </div>
                 </div>
               </div>
-              <div className="w-full flex justify-between text-[11px] font-semibold">
-                <p>Total Pembayaran</p>
-                <p>Rp300.000</p>
+
+              {/* Payment Gate */}
+              <div className="w-full flex flex-wrap gap-3 lg:gap-5">
+                <h2 className="w-full font-semibold lg:text-2xl">Bayar Melalui</h2>
+                <select className="select select-sm border-2 box-content border-primary lg:select-md w-44 lg:text-base">
+                  <option>Transfer Bank</option>
+                  <option>Tunai</option>
+                </select>
+                <p className="text-[11px] w-full text-justify lg:text-sm">
+                  *Pastikan memilih opsi dengan benar sebab opsi yang telah
+                  dipilih tidak dapat dirubah, setelah memilih opsi, segera
+                  lakukan pembayaran
+                </p>
+              </div>
+
+              {/* Pay Button */}
+              <div className="w-full flex justify-center">
+                <button className="bg-primary text-white font-semibold text-xs rounded-full w-1/2 hover:text-primary hover:bg-secondary duration-75 ease-in py-2 lg:text-lg lg:py-3">
+                  Bayar kos
+                </button>
               </div>
             </div>
           </div>
-
-          
         </div>
       </div>
     </div>
--- src/components/History/historySidebar.jsx
+++ src/components/History/historySidebar.jsx
@@ -1,45 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink } from "react-router-dom";
 
 function HistorySidebar() {
   const activeLinkClassname = "text-primary";
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       {/* NavLink */}
-      <ul className="w-[40%] text-sm font-medium text-slate-400 flex flex-wrap gap-3 history-navlink content-start">
-        <li>
-          <NavLink
-            to={"/history/reservation-info"}
-            className={({ isActive }) => isActive && activeLinkClassname}
-          >
-            Pemesanan Kos
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to={"/"}
-            className={({ isActive }) => isActive && activeLinkClassname}
-          >
-            Menunggu Konfirmasi
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to={"/"}
-            className={({ isActive }) => isActive && activeLinkClassname}
-          >
-            Pembayaran
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to={"/"}
-            className={({ isActive }) => isActive && activeLinkClassname}
-          >
-            Pembatalan
-          </NavLink>
-        </li>
-      </ul>
+      <div className={`w-[50%] font-medium text-slate-400 flex flex-wrap fixed bg-white  rounded-xl overflow-hidden drop-shadow-xl top-56 left-0 -translate-y-1/2 ${isOpen ? '-translate-x-[90%]' : '-translate-x-0'} duration-200 ease-in-out max-w-[270px] lg:static lg:w-[30%] lg:translate-x-0 lg:translate-y-0 lg:text-xl lg:rounded-b-none lg:max-w-none lg:px-4 lg:bg-transparent lg:drop-shadow-none`}>
+        <ul className="flex flex-wrap gap-3 history-navlink content-start w-[90%] px-3 py-4 lg:w-full">
+          <li>
+            <NavLink
+              to={"/history/reservation-info"}
+              className={({ isActive }) => isActive && activeLinkClassname}
+            >
+              Pemesanan Kos
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) => isActive && activeLinkClassname}
+            >
+              Menunggu Konfirmasi
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) => isActive && activeLinkClassname}
+            >
+              Pembayaran
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) => isActive && activeLinkClassname}
+            >
+              Pembatalan
+            </NavLink>
+          </li>
+        </ul>
+        <button className="w-[10%] bg-contrary flex items-center justify-center lg:hidden" onClick={() => {setIsOpen((prev) => !prev)}}>
+            <p className="-rotate-90 text-xs text-slate-900">Menu</p>
+        </button>
+      </div>
     </>
   );
 }
